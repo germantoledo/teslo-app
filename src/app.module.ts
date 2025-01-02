6e6ddf4b036ea29f099c46a0f0a29205ab6dbb4e
germantoledo <gefetoledo@gmail.com>
2025-01-02 13:49:01 -0400

    carga de archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,9 +22,14 @@ import { SeedModule } from './seed/seed.module';
       autoLoadEntities: true,
       synchronize: true, // este por lo general en produccion va en false
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..','public'),
+    }),
     ProductsModule,
     CommonModule,
     SeedModule,
+    FilesModule,
+    AuthModule,
   ],
 })
 export class AppModule { }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,7 @@ import { DataSource, Repository } from 'typeorm';
 import { Product, ProductImage } from './entities';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { validate as isUUID } from 'uuid';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class ProductsService {
@@ -22,13 +23,14 @@ export class ProductsService {
     private readonly dataSource: DataSource,
   ) { }
 
-  async create(createProductDto: CreateProductDto) {
+  async create(createProductDto: CreateProductDto, user: User) {
     try {
       const {images = [], ...productDetails} = createProductDto;
 
       const product = this.productRepository.create({
         ...productDetails,
-        images: images.map(image => this.productImageRepository.create({url: image}))
+        images: images.map(image => this.productImageRepository.create({url: image})),
+        user
       });
 
       await this.productRepository.save(product);
@@ -86,7 +88,7 @@ export class ProductsService {
     }
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto) {
+  async update(id: string, updateProductDto: UpdateProductDto, user: User) {
 
     const {images, ...toUpdate} = updateProductDto;
 
@@ -110,6 +112,9 @@ export class ProductsService {
 
         product.images = images.map(image => this.productImageRepository.create({url: image}))
       }
+
+
+      product.user = user;
 
       await queryRunner.manager.save(product);
       // await this.productRepository.save(product);
