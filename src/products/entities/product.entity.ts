0b14a56fb236824f6c78d49f6134f5537bd1f327
germantoledo <gefetoledo@gmail.com>
2025-01-02 14:21:12 -0400

    swagger

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,47 +1,72 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
 import { User } from "src/auth/entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({name: 'products'})
 export class Product {
 
+    @ApiProperty({
+        example: 'UUasdasdq929034',
+        description: 'Product ID ',
+        uniqueItems: true
+    })
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @ApiProperty({
+        example: 'T-shirt Teslo',
+        description: 'Product Title',
+        uniqueItems: true
+    })
     @Column('text', {
         unique: true,
     })
     title: string;
 
+    @ApiProperty({
+        example: 0,
+        description: 'Product Price'
+    })
     @Column('float', {
         default: 0
     })
     price: number;
 
+    @ApiProperty({
+        example: 'Lorem ipsumn',
+        description: 'Product Description',
+        default: null
+    })
     @Column({
         type: 'text',
         nullable: true
     })
     description: string;
 
+    @ApiProperty()
     @Column('text', {
         unique: true
     })
     slug: string;
 
+    @ApiProperty()
     @Column('int', {
         default: 0
     })
     stock: number;
 
+    @ApiProperty()
     @Column('text', {
         array: true
     })
     sizes: string[];
 
+    @ApiProperty()
     @Column('text')
     gender: string;
 
+    @ApiProperty()
     @Column({
         type: 'text',
         array: true,
@@ -49,6 +74,7 @@ export class Product {
     })
     tags: string[];
 
+    @ApiProperty()
     @OneToMany(
         () => ProductImage,
         (productImage) => productImage.product,
@@ -56,6 +82,7 @@ export class Product {
     )
     images?: ProductImage[];
 
+    @ApiProperty()
     @ManyToOne(
         () => User,
         (user) => user.product,
